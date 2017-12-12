dca6d0a733d3781432ff9f8db83e4faf04b88ca8
shipperizer <alexcabb@gmail.com>
2017-12-12 12:21:40 +0000

    [++] add mongo and rabbit to acr

--- acr_push.sh
+++ acr_push.sh
@@ -10,7 +10,12 @@ docker-compose pull
 docker tag shanepeckham/captureorderack:v3 $ACR/shanepeckham/captureorderack:v3
 docker tag shanepeckham/fulfillorderack:v3 $ACR/shanepeckham/fulfillorderack:v3
 docker tag shanepeckham/rabbitmqlistenerack:v3 $ACR/shanepeckham/rabbitmqlistenerack:v3
+docker tag rabbitmq:3-management $ACRmongo/rabbitmq:3-management
+docker tag mongo:jessie $ACRmongo/mongo:jessie
 
+# docker pushing to ACR
 docker push $ACR/shanepeckham/captureorderack:v3
 docker push $ACR/shanepeckham/fulfillorderack:v3
 docker push $ACR/shanepeckham/rabbitmqlistenerack:v3
+docker push $ACRmongo/rabbitmq:3-management
+docker push $ACRmongo/mongo:jessie
